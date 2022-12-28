a450976c6604c41931aba55b52a00d94fa52d302
samimghafoori <117555185+samimghafoori@users.noreply.github.com>
2022-12-27 20:49:09 -0800

    Update Samim.java
    change

--- src/samim/Samim.java
+++ src/samim/Samim.java
@@ -5,7 +5,7 @@ public class Samim {
 	public static void main(String[] args) {
 		
 		System.out.println("this is from Eclipes for Samim Class");
-		System.out.println("this is inside the eclipes Branch");
+		System.out.println("this is inside the eclipes Branch for all the time");
 
 	}
 
